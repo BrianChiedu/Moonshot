dfddb0f1ede10cc7674707f5d09591317ad437f7
Brian Chukwuisiocha <brianchukwuisiocha@gmail.com>
2024-03-04 13:04:38 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Brian Chukwuisiocha on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
